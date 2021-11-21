1ec9bf0a38b248661b65c290eadccf87a8786731
ItsSaimi <ah.saim786@gmail.com>
2021-11-21 21:35:43 +0500

    Project4:Rock,Paper and Scissors(GAME)

--- rock_paper_scissors.py
+++ rock_paper_scissors.py
@@ -39,12 +39,12 @@ print(game_images[computer_choice])
 if user_choice >= 3 or user_choice < 0: 
   print("You typed an invalid number, you lose!") 
 elif user_choice == 0 and computer_choice == 2:
-  print("You win!")
+  print("HURRRAY!!!...You win!")
 elif computer_choice == 0 and user_choice == 2:
-  print("You lose")
+  print("ALAASSS!!!...You lose")
 elif computer_choice > user_choice:
-  print("You lose")
+  print("Hump!!!...You lose")
 elif user_choice > computer_choice:
-  print("You win!")
+  print("Yahoo!!!...You win!")
 elif computer_choice == user_choice:
   print("It's a draw")
