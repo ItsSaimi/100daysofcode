5ea0fe071d79f9744bd643c50efccec31614911b
ItsSaimi <ah.saim786@gmail.com>
2021-11-30 01:31:33 +0500

    Sum of Even

--- day6p5.py
+++ day6p5.py
@@ -1,5 +1,13 @@
-t = 0
-for n in range (2, 101, 2):
-    t += n
-    print(t)
-print(f"The sum of even numbers is {t}.")
+#METHOD1
+#t = 0
+#for n in range (2, 101, 2):
+ #   t += n
+  #  print(t)
+#print(f"The sum of even numbers is {t}.")
+
+#METHOD2
+t_2 = 0
+for n in range (1, 101):
+    if n % 2 == 0 :
+        t_2 += n
+print(t_2)
