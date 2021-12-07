b21c31d1feb57bfe6219512282e1fd235a4ef8b6
ItsSaimi <ah.saim786@gmail.com>
2021-12-07 20:55:58 +0500

    Hurdle Challenge

---
+++ day7p3.py
@@ -0,0 +1,16 @@
+def my_sol():
+    move()
+    turn_left()
+    move()
+    turn_left()
+    turn_left()
+    turn_left()
+    move()
+    turn_left()
+    turn_left()
+    turn_left()
+    move()
+    turn_left()
+for step in range(6):
+    my_sol()
+
