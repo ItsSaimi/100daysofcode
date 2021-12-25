cb0494069fad2866537b66dc3c6ebb8c6e1595b1
ItsSaimi <ah.saim786@gmail.com>
2021-12-26 00:02:28 +0500

    Cipher Text

--- EN_DE_code.py
+++ EN_DE_code.py
@@ -1,31 +1,21 @@
-alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z','a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
 direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
 text = input("Type your message:\n").lower()
 shift = int(input("Type the shift number:\n"))
 
+#TODO-1: Combine the encrypt() and decrypt() functions into a single function called caesar(). 
 
-def encrypt(p_text, shift_a):
-    cipher_text=""
-    for letter in p_text:
-        p = alphabet.index(letter)
-        n_p = p + shift_a
-        new_letter = alphabet[n_p]
-        cipher_text += new_letter
-    print(f"Encrypted text is '{cipher_text}'.")
+def caesar(start_text, shift_amount, cipher_direction):
+  end_text = ""
+  if cipher_direction == "decode":
+      shift_amount *= -1
+  for letter in start_text:
+    position = alphabet.index(letter)
+    new_position = position + shift_amount
+    end_text += alphabet[new_position]
+  print(f"Here's the {direction}d result: {end_text}")
 
-#encrypt(p_text=text, shift_a=shift)
 
-def decrypt(cipher_text, shift_a):
-    p_text = ""
-    for letter in cipher_text:
-        p = alphabet.index(letter)
-        n_p = p - shift_a
-        new_letter = alphabet[n_p]
-        p_text += new_letter
-    print(f"Decrypted text is '{p_text}'.")
-
-if direction == "encode":
-  encrypt(p_text=text, shift_a=shift)
-elif direction == "decode":
-  decrypt(cipher_text=text, shift_adecode=shift)
+#TODO-2: Call the caesar() function, passing over the 'text', 'shift' and 'direction' values.
+caesar(start_text=text, shift_amount=shift, cipher_direction=direction)
