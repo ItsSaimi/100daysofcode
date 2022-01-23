2f0f2e4c0d2285daf40c62b4b2dd98bd72679298
ItsSaimi <ah.saim786@gmail.com>
2022-01-24 03:16:30 +0500

    Calculator

--- _Calculator.py
+++ _Calculator.py
@@ -16,15 +16,24 @@ operation={
     "*" : multiply,
     "/" : divide
 }
+
+
 num1 = int(input("Enter the first number:"))
- 
-for symbol in operation:
-    print(symbol)
-operation_function = input("Select the operation from above:")
-num2 = int(input("Enter the second number:"))
-c = operation[operation_function]
-ans1 = c(num1, num2)
-print(f"{num1}{operation_function}{num2} = {ans1}")
+
+
+should_continue=True
+while should_continue:
+    for symbol in operation:
+        print(symbol)
+    operation_function = input("Select the operation:")
+    num2 = int(input("Enter the next number:"))
+    c = operation[operation_function]
+    ans = c(num1, num2)
+    print(f"{num1}{operation_function}{num2} = {ans}")
+    if input(f"Type 'y' to continue calculating with {ans}, or type 'n' to start a new calculation: ")=='y':
+        num1=ans
+    else:
+      should_continue = False
 
 # operation_function = input("Select the operation from above:")
 # num3 = input("Enter the second number")
