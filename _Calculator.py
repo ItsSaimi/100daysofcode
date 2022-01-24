47ad0edaeb842e190aa313c1b8e0f9faa5cb3aec
ItsSaimi <ah.saim786@gmail.com>
2022-01-24 05:53:23 +0500

    Calculator

--- _Calculator.py
+++ _Calculator.py
@@ -18,24 +18,28 @@ operation={
 }
 
 
-num1 = int(input("Enter the first number:"))
+def cal():
+    from cal_art import logo
+    print(logo)
+    num1 = float(input("Enter the first number:"))
 
 
-should_continue=True
-while should_continue:
-    for symbol in operation:
-        print(symbol)
-    operation_function = input("Select the operation:")
-    num2 = int(input("Enter the next number:"))
-    c = operation[operation_function]
-    ans = c(num1, num2)
-    print(f"{num1}{operation_function}{num2} = {ans}")
-    if input(f"Type 'y' to continue calculating with {ans}, or type 'n' to start a new calculation: ")=='y':
-        num1=ans
-    else:
-      should_continue = False
+    should_continue=True
+    while should_continue:
+        for symbol in operation:
+            print(symbol)
+        operation_function = input("Select the operation:")
+        num2 = float(input("Enter the next number:"))
+        c = operation[operation_function]
+        ans = c(num1, num2)
+        print(f"{num1}{operation_function}{num2} = {ans}")
+        if input(f"Type 'y' to continue calculating with {ans}, or type 'n' to start a new calculation: ")=='y':
+            num1=ans
+        else:
+            should_continue = False
+            cal()
+cal()  
 
-# operation_function = input("Select the operation from above:")
-# num3 = input("Enter the second number")
+
 
 
